4653c1a8ba62b211b92e8076b97b6d104634601e
deepanshiruhil <178875578+deepanshiruhil@users.noreply.github.com>
2026-06-06 09:11:47 +0530

    consistency enhanced in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+// ESLint configuration for deepanshiruhil
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -8,7 +9,6 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
